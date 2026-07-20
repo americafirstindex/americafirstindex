a84c28fd0d3055b8225ca57e76a2f51d8be18580
EW <ew@EWs-MacBook-Air.local>
2026-07-19 19:29:53 -0500

    cont. states

--- scripts/simplify-geo.js
+++ scripts/simplify-geo.js
@@ -24,16 +24,17 @@ const HIGH_QUALITY = false; // faster Radial Distance pre-pass is fine here
 
 const STATE_ABBR = {
   alabama: 'AL', arizona: 'AZ', arkansas: 'AR', california: 'CA',
-  colorado: 'CO', connecticut: 'CT', florida: 'FL', georgia: 'GA',
-  idaho: 'ID', illinois: 'IL', indiana: 'IN', iowa: 'IA', kansas: 'KS',
-  kentucky: 'KY', louisiana: 'LA', maine: 'ME', maryland: 'MD',
+  colorado: 'CO', connecticut: 'CT', delaware: 'DE', florida: 'FL',
+  georgia: 'GA', idaho: 'ID', illinois: 'IL', indiana: 'IN', iowa: 'IA',
+  kansas: 'KS', kentucky: 'KY', louisiana: 'LA', maine: 'ME', maryland: 'MD',
   massachusetts: 'MA', michigan: 'MI', minnesota: 'MN', mississippi: 'MS',
   missouri: 'MO', montana: 'MT', nebraska: 'NE', nevada: 'NV',
   newhampshire: 'NH', newjersey: 'NJ', newmexico: 'NM', newyork: 'NY',
-  northcarolina: 'NC', ohio: 'OH', oklahoma: 'OK', oregon: 'OR',
-  pennsylvania: 'PA', rhodeisland: 'RI', southcarolina: 'SC',
-  tennessee: 'TN', texas: 'TX', utah: 'UT', virginia: 'VA',
-  washington: 'WA', westvirginia: 'WV', wisconsin: 'WI',
+  northcarolina: 'NC', northdakota: 'ND', ohio: 'OH', oklahoma: 'OK',
+  oregon: 'OR', pennsylvania: 'PA', rhodeisland: 'RI', southcarolina: 'SC',
+  southdakota: 'SD', tennessee: 'TN', texas: 'TX', utah: 'UT',
+  vermont: 'VT', virginia: 'VA', washington: 'WA', westvirginia: 'WV',
+  wisconsin: 'WI', wyoming: 'WY',
 };
 
 function round4(n) {
